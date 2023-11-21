822e6ac98a0a958f0b427d45262594e1f1c99f7e
Vince Khoa Dinh <39015239+vincedinh@users.noreply.github.com>
2023-11-21 01:29:35 -0800

    Fixed some stylings

--- src/ui/dashboard/navLinks.tsx
+++ src/ui/dashboard/navLinks.tsx
@@ -28,9 +28,12 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-cream-500 p-3 text-sm font-medium hover:bg-cream-500 hover:text-orange-400 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': link.href === pathname,
+                'bg-cream-600 text-orange-500': link.href === pathname,
+              },
+              {
+                'bg-cream-500 text-black-400': link.href !== pathname,
               },
             )}          >
             <LinkIcon className="w-6" />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,13 +3,41 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+      },
+      colors: {
+        blue: {
+          400: '#2589FE',
+          500: '#0070F3',
+          600: '#2F6FEB',
+        },
+        orange: {
+          400: '#dfa055',
+          500: '#c07228',
+          600: '#a55100',
+        },
+        cream: {
+          400: '#f8f6ef',
+          500: '#edede5',
+          600: '#e7e4dd',
+        },
+        black: {
+          400: '#140d08',
+          500: '#0c0808',
+          600: '#000000'
+        }
+      },
+    },
+    keyframes: {
+      shimmer: {
+        '100%': {
+          transform: 'translateX(100%)',
+        },
       },
     },
   },
